132d50c6e06ea98afa6b67d03ef1cecf41c96d66
wonki <wonki95891@gmail.com>
2024-01-03 20:31:46 +0900

    develop product domain

--- src/main/java/com/example/jpaProject/domain/item/Item.java
+++ src/main/java/com/example/jpaProject/domain/item/Item.java
@@ -1,5 +1,6 @@
 package com.example.jpaProject.domain.item;
 
+import com.example.jpaProject.exception.NotEnoughStockException;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -25,4 +26,18 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // 비지니스 로직
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity ;
+        if(restStock < 0 ){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
 }
