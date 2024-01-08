771214543810dc01a96e9196f539a31e4a9a1f3c
wonki <wonki95891@gmail.com>
2024-01-08 18:04:29 +0900

    connect react

--- src/main/java/com/example/jpaProject/controller/HomeController.java
+++ src/main/java/com/example/jpaProject/controller/HomeController.java
@@ -7,12 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @Slf4j
 public class HomeController {
 
     @GetMapping("/api/hello")
-    public String test() {
-        return "Hello, world!";
+    public List<String> test() {
+        return Arrays.asList("hello","test022");
     }
 }
