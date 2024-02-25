7756d8c5252ece4b0181762eedf205b4bf295890
wonki <wonki95891@gmail.com>
2024-02-25 11:56:27 +0900

    주석제거

--- src/main/java/jpaProject/jpashop/controller/ProductDTO.java
+++ src/main/java/jpaProject/jpashop/controller/ProductDTO.java
@@ -1,18 +1,8 @@
 package jpaProject.jpashop.controller;
-
-import io.hypersistence.utils.hibernate.type.json.JsonType;
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jpaProject.jpashop.domain.item.Product;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.Type;
-
 import java.util.List;
-import java.util.Map;
+
 
 @Getter @Setter
 public class ProductDTO {
