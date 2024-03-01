aea1164615785a613c004ee65c5a817258fb80d0
wonki <wonki95891@gmail.com>
2024-03-01 18:34:12 +0900

    setting admin controller

--- src/main/java/jpaProject/jpashop/controller/AdminController.java
+++ src/main/java/jpaProject/jpashop/controller/AdminController.java
@@ -1,16 +1,32 @@
 package jpaProject.jpashop.controller;
 
+import jpaProject.jpashop.domain.Member;
+import jpaProject.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
-@ResponseBody
+@RequiredArgsConstructor
+
+
 public class AdminController {
+
+    private final MemberService memberService;
 
     @GetMapping("/api/admin")
     public String adminP() {
-
         return "admin Controller";
+    }
+
+    @ResponseBody
+    @GetMapping("/api/admin/members")
+    public ResponseEntity findMembers(){
+        List<Member> members = memberService.findMembers();
+        return ResponseEntity.ok(members);
     }
 }
